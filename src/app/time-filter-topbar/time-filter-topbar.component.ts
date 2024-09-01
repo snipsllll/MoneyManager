058dff1c988c4a1f53c968dbaa80fdd7f4e9a271
Tümmers, David <david.tuemmers@connext.de>
2024-09-01 23:43:21 +0200

    active zeit filter anzeige

--- src/app/time-filter-topbar/time-filter-topbar.component.ts
+++ src/app/time-filter-topbar/time-filter-topbar.component.ts
@@ -1,20 +1,45 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-time-filter-topbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './time-filter-topbar.component.html',
   styleUrl: './time-filter-topbar.component.css'
 })
 export class TimeFilterTopbarComponent {
 
-  @Output() onHeuteClicked = new EventEmitter();
+  activeClass = 'alle';
 
-  @Output() onWocheClicked = new EventEmitter();
+  @Output() onHeuteClickedEvnt = new EventEmitter();
 
-  @Output() onMonatClicked = new EventEmitter();
+  @Output() onWocheClickedEvnt = new EventEmitter();
 
-  @Output() onAlleClicked = new EventEmitter();
+  @Output() onMonatClickedEvnt = new EventEmitter();
+
+  @Output() onAlleClickedEvnt = new EventEmitter();
+
+  onHeuteClicked(){
+    this.activeClass = 'heute';
+    this.onHeuteClickedEvnt.emit()
+  }
+
+  onWocheClicked(){
+    this.activeClass = 'woche';
+    this.onWocheClickedEvnt.emit()
+  }
+
+  onMonatClicked(){
+    this.activeClass = 'monat';
+    this.onMonatClickedEvnt.emit()
+  }
+
+  onAlleClicked() {
+    this.activeClass = 'alle';
+    this.onAlleClickedEvnt.emit()
+  }
 
 }
