83a5301d61f4db399c542b3fb09107341f62992d
Tümmers, David <david.tuemmers@connext.de>
2024-09-07 13:02:45 +0200

    fixkosten ui angefangen

--- src/app/fix-kosten/fix-kosten.component.ts
+++ src/app/fix-kosten/fix-kosten.component.ts
@@ -1,23 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {TopBarComponent} from "../top-bar/top-bar.component";
 import {TopbarService} from "../topbar.service";
+import {NgForOf} from "@angular/common";
+import {Data} from "@angular/router";
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-fix-kosten',
   standalone: true,
   imports: [
-    TopBarComponent
+    TopBarComponent,
+    NgForOf
   ],
   templateUrl: './fix-kosten.component.html',
   styleUrl: './fix-kosten.component.css'
 })
 export class FixKostenComponent implements OnInit{
 
-  constructor(private topbarService: TopbarService) {
+  constructor(private topbarService: TopbarService, public dataService: DataService) {
   }
 
   ngOnInit() {
     this.topbarService.title.set('FIX KOSTEN');
+    this.topbarService.dropDownSlidIn.set(false);
+    this.topbarService.isDropDownDisabled = true;
+  }
+
+  onPlusClicked() {
+
   }
 
 }
