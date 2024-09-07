e3142179be264a3904cc0e4b620ac1cfb09bb5f7
Tümmers, David <david.tuemmers@connext.de>
2024-09-07 12:26:28 +0200

    topbar und side nav

--- src/app/fix-kosten/fix-kosten.component.ts
+++ src/app/fix-kosten/fix-kosten.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TopBarComponent} from "../top-bar/top-bar.component";
+import {TopbarService} from "../topbar.service";
 
 @Component({
   selector: 'app-fix-kosten',
   standalone: true,
-  imports: [],
+  imports: [
+    TopBarComponent
+  ],
   templateUrl: './fix-kosten.component.html',
   styleUrl: './fix-kosten.component.css'
 })
-export class FixKostenComponent {
+export class FixKostenComponent implements OnInit{
+
+  constructor(private topbarService: TopbarService) {
+  }
+
+  ngOnInit() {
+    this.topbarService.title.set('FIX KOSTEN');
+  }
 
 }
