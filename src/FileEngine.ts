cad0377f3dfc308613410f179f3dc50022963975
Tümmers, David <david.tuemmers@connext.de>
2024-09-07 09:46:28 +0200

    better testData

--- src/FileEngine.ts
+++ src/FileEngine.ts
@@ -58,24 +58,168 @@ export class FileEngine {
   }
 
   private getTestData(): SavedData {
+    const day1 = new Date();
+    const day2 = new Date()
+    const day3 = new Date()
+    const day4 = new Date()
+    const day5 = new Date()
+    const day6 = new Date()
+    const day7 = new Date()
+    const day8 = new Date()
+    const day9 = new Date()
+    day2.setDate(day1.getDate() - 1);
+    day3.setDate(day1.getDate() - 2);
+    day4.setDate(day1.getDate() - 3);
+    day5.setDate(day1.getDate() - 4);
+    day6.setDate(day1.getDate() - 5);
+    day7.setDate(day1.getDate() - 6);
+    day8.setDate(day1.getDate() - 7);
+    day9.setDate(day1.getDate() - 8);
+    switch (this.useTestData) {
+      case 0:
+        return {
+          buchungen: [
+            {
+              date: new Date(),
+              id: 1,
+              title: 'test titel',
+              betrag: 2,
+              time: new Date().toLocaleTimeString(),
+              beschreibung: 'testbeschreibung'
+            }
+          ],
+          savedMonths: [
+            {
+              date: new Date(),
+              sparen: 100,
+              totalBudget: 400
+            }
+          ]
+        }
+      case 1:
+        return {
+          buchungen: [
+            {
+              date: day1,
+              id: 1,
+              title: 'test titel',
+              betrag: 2,
+              time: '08:00',
+              beschreibung: 'testbeschreibung'
+            },
+            {
+              date: day1,
+              id: 2,
+              title: 'test titel 2',
+              betrag: 5.5,
+              time: '08:01',
+              beschreibung: 'testbeschreibung 2'
+            },
+            {
+              date: day2,
+              id: 3,
+              title: 'test titel 3',
+              betrag: 12,
+              time: '08:00',
+              beschreibung: 'testbeschreibung 3'
+            },
+            {
+              date: day2,
+              id: 4,
+              title: 'test titel 4',
+              betrag: 2.98,
+              time: '18:30',
+              beschreibung: 'testbeschreibung 4'
+            },
+            {
+              date: day3,
+              id: 5,
+              title: 'test titel 5',
+              betrag: 21,
+              time: '12:03',
+              beschreibung: 'testbeschreibung 5'
+            }
+          ],
+          savedMonths: [
+            {
+              date: new Date(),
+              sparen: 100,
+              totalBudget: 400
+            }
+          ]
+        }
+      case 2:
+        return {
+          buchungen: [
+            {
+              date: day1,
+              id: 1,
+              title: 'test titel',
+              betrag: 2,
+              time: '08:00',
+              beschreibung: 'testbeschreibung'
+            },
+            {
+              date: day1,
+              id: 2,
+              title: 'test titel 2',
+              betrag: 5.5,
+              time: '08:01',
+              beschreibung: 'testbeschreibung 2'
+            },
+            {
+              date: day1,
+              id: 3,
+              title: 'test titel 3',
+              betrag: 1.56,
+              time: '08:26',
+              beschreibung: 'testbeschreibung 3'
+            },
+            {
+              date: day1,
+              id: 4,
+              title: 'test titel 4',
+              betrag: 2,
+              time: '17:32',
+              beschreibung: 'testbeschreibung 4'
+            },
+            {
+              date: day2,
+              id: 5,
+              title: 'test titel 5',
+              betrag: 12,
+              time: '08:00',
+              beschreibung: 'testbeschreibung 5'
+            },
+            {
+              date: day2,
+              id: 6,
+              title: 'test titel 6',
+              betrag: 2.98,
+              time: '18:30',
+              beschreibung: 'testbeschreibung 6'
+            },
+            {
+              date: day3,
+              id: 7,
+              title: 'test titel 7',
+              betrag: 21,
+              time: '12:03',
+              beschreibung: 'testbeschreibung 7'
+            }
+          ],
+          savedMonths: [
+            {
+              date: new Date(),
+              sparen: 100,
+              totalBudget: 400
+            }
+          ]
+        }
+    }
     return {
-      buchungen: [
-        {
-          date: new Date(),
-          id: 1,
-          title: 'test titel',
-          betrag: 2,
-          time: new Date().toLocaleTimeString(),
-          beschreibung: 'testbeschreibung'
-        }
-      ],
-      savedMonths: [
-        {
-          date: new Date(),
-          sparen: 100,
-          totalBudget: 400
-        }
-      ]
+      buchungen: [],
+      savedMonths: []
     }
   }
 }
