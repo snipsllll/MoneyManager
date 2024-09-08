00c40c78e3b64a40b05468f3d26db7aa20cf732a
Tümmers, David <david.tuemmers@connext.de>
2024-09-08 20:46:25 +0200

    test overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,87 @@ import {FormsModule} from "@angular/forms";
 import {EditBuchungComponent} from "./edit-buchung/edit-buchung.component";
 import {CreateBuchungComponent} from "./create-buchung/create-buchung.component";
 import {TopBarComponent} from "./top-bar/top-bar.component";
+import {MmButtonComponent} from "./mm-button/mm-button.component";
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet, HomeComponent, ConfirmDialogComponent, BuchungDetailsComponent, FormsModule, EditBuchungComponent, CreateBuchungComponent, TopBarComponent],
+  imports: [RouterModule, RouterOutlet, HomeComponent, ConfirmDialogComponent, BuchungDetailsComponent, FormsModule, EditBuchungComponent, CreateBuchungComponent, TopBarComponent, MmButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit{
   title = 'MoneyManager';
 
-  constructor(private router: Router, public dialogService: DialogService) {
+  constructor(private dataService: DataService, private router: Router, public dialogService: DialogService) {
   }
 
   ngOnInit() {
     this.router.navigate(['/'])
+  }
+
+  createMonth() {
+    this.dataService.createNewMonth(new Date());
+  }
+
+  changeSparen() {
+    this.dataService.update({
+      months: [
+        {
+          date: new Date(),
+          newSparen: 100
+        }
+      ]
+    })
+  }
+
+  changeTotalBudget() {
+    this.dataService.update({
+      months: [
+        {
+          date: new Date(),
+          newTotalBudget: 1000
+        }
+      ]
+    })
+  }
+
+  newBuchung() {
+    this.dataService.update({
+      months: [
+        {
+          date: new Date(),
+          newBuchungen: [
+            {
+              date: new Date(),
+              betrag: 40,
+              title: '',
+              time: new Date().toLocaleTimeString()
+            }
+          ]
+        }
+      ]
+    })
+  }
+
+  newFixkostenEintrag() {
+    this.dataService.update({
+      months: [
+        {
+          date: new Date(),
+          newFixkostenEintraege: [
+            {
+              betrag: 50,
+              title: ''
+            }
+          ]
+        }
+      ]
+    })
+  }
+
+  logUserData() {
+    console.log(this.dataService.userData)
   }
 }
